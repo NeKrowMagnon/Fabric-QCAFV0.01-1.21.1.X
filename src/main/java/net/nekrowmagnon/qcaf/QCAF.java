8fde54e65dcb0144a385912ce24324ce07e41f4d
NeKrowMagnon <krowmagnon2@gmail.com>
2024-09-05 19:08:54 -0400

    Initial commit

---
+++ src/main/java/net/nekrowmagnon/qcaf/QCAF.java
@@ -0,0 +1,16 @@
+package net.nekrowmagnon.qcaf;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QCAF implements ModInitializer {
+	public static final String MOD_ID = "qcaf";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/nekrowmagnon/qcaf/QCAFClient.java
@@ -0,0 +1,10 @@
+package net.nekrowmagnon.qcaf;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QCAFClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
